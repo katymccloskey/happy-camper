1ec49986b05c75319ced725a1ef2f774bc44a26e
Mitchell Hendee <mitchellhendee@gmail.com>
2017-08-19 16:56:43 -0500

    amenities assigns time to deal with a detail hash from hell

--- app/controllers/campgrounds_controller.rb
+++ app/controllers/campgrounds_controller.rb
@@ -2,7 +2,11 @@ class CampgroundsController < ApplicationController
   def show
     @campground = Campground.find(params[:id])
     if @campground.detail.nil?
-      detail = Supercamp.details.contract_code(@campground.contract_id).id(@campground.facility_id).results
-    @campground.detail << Detail.find_or_create_by(description: detail[:])
-  end
-end
+      details = Supercamp.details.contract_code(@campground.contract_id).id(@campground.facility_id).results
+      details.each do |detail|
+       amenity = Amenity.find_or_create_by(name: detail[:name]) if detail[:distance] && detail[:name]
+       @campground.amenities << amenity
+
+       @campground.detail << Detail.create(description: detail[:])
+     end
+   end
