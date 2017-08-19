8bbfb29cfdab87a1d92c01ec9adb0bee46c0dbc2
Mitchell Hendee <mitchellhendee@gmail.com>
2017-08-19 12:39:16 -0500

    adjust migration to handle string

--- db/migrate/20170818221449_create_campgrounds.rb
+++ db/migrate/20170818221449_create_campgrounds.rb
@@ -1,16 +1,16 @@
 class CreateCampgrounds < ActiveRecord::Migration[5.1]
   def change
     create_table :campgrounds do |t|
-      t.string :name, null: false, limit: 50
-      t.string :state, null: false, limit: 2
+      t.string :name, null: false, limit: 75
+      t.string :state, null: false, limit: 4
       t.string :contract_id, null: false
       t.string :facility_id, null: false
       t.float :latitude
       t.float :longitude
-      t.boolean :amps
-      t.boolean :pets
-      t.boolean :sewage
-      t.boolean :water
+      t.string :amps
+      t.string :pets
+      t.string :sewage
+      t.string :water
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# things.each do |camp|
+#   Campground.create(name: camp[:facility_name], state: camp[:state], contract_id: camp[:contract_id],
+#     facility_id: camp[:facility_id], latitude: camp[:latitude], longitude: camp[:longitude],
+#   amps: true)
+#  end
