2ce2e9f7b183f23b567fa7c2f455c149fe09f8f3
Mitchell Hendee <mitchellhendee@gmail.com>
2017-08-19 18:30:44 -0500

    first attempt at detail integration

--- app/controllers/campgrounds_controller.rb
+++ app/controllers/campgrounds_controller.rb
@@ -3,10 +3,25 @@ class CampgroundsController < ApplicationController
     @campground = Campground.find(params[:id])
     if @campground.detail.nil?
       details = Supercamp.details.contract_code(@campground.contract_id).id(@campground.facility_id).results
+      parent = details.find { |hash| hash.keys.include?(:parent) }.reduce[1]
+
       details.each do |detail|
        amenity = Amenity.find_or_create_by(name: detail[:name]) if detail[:distance] && detail[:name]
        @campground.amenities << amenity
-
-       @campground.detail << Detail.create(description: detail[:])
      end
+       prime_detail = Detail.create(city: details[0].city.titleize,
+                                state: details[0].state,
+                                address: details[0].street_address.titleize,
+                                zip: details[0].zip,
+                                description: parent.description,
+                                facilities_description: parent.facilities_description,
+                                important_info: parent.important_information,
+                                recreation_description: parent.recreation_description,
+                                orientation_description: parent.orientation_description,
+                                reservation_url: parent.full_reservation_url,
+                                campground: @campground
+                                 )
+       @campground.detail << prime_detail
+     end
+
    end
--- db/migrate/20170818221511_create_details.rb
+++ db/migrate/20170818221511_create_details.rb
@@ -4,6 +4,9 @@ class CreateDetails < ActiveRecord::Migration[5.1]
       t.string :description, null: false
       t.string :reservation_url
       t.string :important_info
+      t.string :recreation_description
+      t.string :orientation_description
+      t.string :facilities_description
       t.string :recreations
       t.string :city, null: false
       t.string :state, null: false
