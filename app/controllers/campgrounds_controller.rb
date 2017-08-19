484326013a6a086e49eb9efae60fc1d6e6c09561
Mitchell Hendee <mitchellhendee@gmail.com>
2017-08-19 16:22:58 -0500

    add detail variable test

--- app/controllers/campgrounds_controller.rb
+++ app/controllers/campgrounds_controller.rb
@@ -1,5 +1,8 @@
 class CampgroundsController < ApplicationController
   def show
     @campground = Campground.find(params[:id])
+    if @campground.detail.nil?
+      detail = Supercamp.details.contract_code(@campground.contract_id).id(@campground.facility_id).results
+    @campground.detail << Detail.find_or_create_by(description: detail[:])
   end
 end
