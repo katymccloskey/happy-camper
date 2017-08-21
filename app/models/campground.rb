f22b09845dbbc6c60d4af6cd199829f85ffb5276
Mitchell Hendee <mitchellhendee@gmail.com>
2017-08-21 14:37:31 -0500

    add long winded or call to campground model

--- app/models/campground.rb
+++ app/models/campground.rb
@@ -5,4 +5,33 @@ class Campground < ApplicationRecord
   has_many :cg_amenities
   has_many :amenities, through: :cg_amenities
   has_one :detail
+
+  def find_amenity_accessible
+    self.amenities.each do |amenity|
+      amenity.name if amenity.name.downcase.include? "accessible"
+    end
+  end
+
+  def find_amenity_fire
+    self.amenities.each do |amenity|
+      amenity.name if amenity.name.downcase.include? "campfire" || "fire" || "firewood" || "log"
+    end
+  end
+
+  def find_amenity_water
+    self.amenities.each do |amenity|
+      amenity.name if amenity.name.downcase.include? "water" || "pump" || "toilet" || "drinking" || "sink"
+    end
+  end
+
+  def find_amenity_fun
+    self.amenities.each do |amenity|
+      amenity.name if amenity.name.downcase.include? "horse" || "bike" || "kayak" || "boat" || "bird" || "view" || "rafting" || "theater" || "fishing"
+    end
+  end
+  def find_amenity_general
+    self.amenities.each do |amenity|
+      amenity.name if amenity.name.downcase.include? "office" || "otel" || "store" || "shop" || "grocery" || "cafe" || "visitor"
+    end
+  end
 end
