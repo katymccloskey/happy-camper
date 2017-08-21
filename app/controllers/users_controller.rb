d2c20fa4ed616c449008eb4a794fa0ec7d29c1e1
Katy McCloskey <email": null>
2017-08-21 10:50:09 -0500

    favorites button functioning yay

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
 
  def show
   @user = User.find(params[:id])
-  @campground = Campground.all.limit(12)
+  # @campground = Campground.all.limit(12)
   # uncomment as soon as user.favorites is functioning, above line just for example data
-  # @campground = @user.favorites
+  @campground = @user.campgrounds
  end
 
 end
